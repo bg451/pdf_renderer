3a4ddc560d82ce2e6d0ee4cc71ea4361045b2856
Brandon Gonzalez <brandon.gonzalez.451@gmail.com>
2013-08-23 09:24:03 -0700

    PDF renderer plugin for rails

---
+++ test/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dummy::Application.config.secret_key_base = 'f64b51b2c375ee97c52d2388ee293f9a3ddb3c9b130c2f6f52c20f309ad26c02f586dfd774a2fc033f7fecbbd4439b1c1931de6770e9f5a71a17fade990297c9'
